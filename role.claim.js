66487f90e2ce3f613ace25f0959bd2fa49409d46
John Clements <jsclements642@gmail.com>
2016-09-09 20:52:49 -0400

    Fixed diplomat shuffle
    Made it so the creep tries to move to the controller if its in the
    correct room first! Thus solving the room shuffle

--- role.claim.js
+++ role.claim.js
@@ -5,20 +5,19 @@ var claimer = {
         // todo:: Add a if else wrapper to this that blocks pickup attempts if a unsafe flag appears.
         
         
-        if(creep.room.name != creep.memory.station){
-            creep.moveTo(creep.pos.findClosestByPath(creep.room.findExitTo(creep.memory.station)));
-            }
-            
         
-        else if(creep.room.controller) {
+        if(creep.room.name == creep.memory.station){
+            
             if(creep.reserveController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 
                 creep.moveTo(creep.room.controller);
-                }
+            }
         }
-        
+            
+        else {
+            creep.moveTo(creep.pos.findClosestByPath(creep.room.findExitTo(creep.memory.station))); 
 
-        
+        }
     }
 }
 
