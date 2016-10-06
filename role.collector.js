e5abe00c5690c50a7530cf49d5a4ea80bc89a0cd
John Clements <jsclements642@gmail.com>
2016-10-06 18:52:02 -0400

    Rebuilt the collector role
    Since it was almost entirely broken in every way. This should probaby
    use some inheritance

--- role.collector.js
+++ role.collector.js
@@ -3,35 +3,36 @@ var roleCollector = {
     /** Collects minerals and stores them, also loots dead creep energy drops **/
     run: function(creep) {
         
-        if(creep.memory.dropoff == undefined){
-            creep.memory.dropoff = false;
+        if(creep.memory.gathering == undefined){
+            creep.memory.gathering = true;
         }
         
         // How much is the creep carrying?
         var total = _.sum(creep.carry);
         
-        if(creep.memory.dropoff){
+        if(creep.memory.gathering){
             // Return everything you have to storeage!
-            var storage = creep.room.storage;
             
-            // If the creep is empty go pick up.
-            if(total == 0){
-                creep.memory.dropoff = false;
+            // If the creep is full go drop off.
+            if(total == creep.carryCapacity){
+                creep.memory.gathering = false;
             }
             // otherwise continue and drop the goods off at the storage
             else{
-                for(var resourceType in creep.carry) {
-                	if(creep.transfer(storage, resourceType) == ERR_NOT_IN_RANGE){
-                	    creep.moveTo(storage);
-                	    break;
-                	}
+                var extractor = creep.room.find(FIND_MINERALS)[0];
+                if(creep.harvest(extractor) == ERR_NOT_IN_RANGE){
+                    creep.moveTo(extractor);
                 }
             }
         }
         else{
-            var extractor = creep.room.find(FIND_MINERALS)[0];
-            if(creep.harvest(extractor) == ERR_NOT_IN_RANGE){
-                creep.moveTo(extractor);
+            if(creep.pos.inRangeTo(creep.room.storage.pos,1)){
+                for(var resourceType in creep.carry) {
+                    creep.transfer(creep.room.storage, resourceType)
+                }
+                
+            } else {
+                creep.moveTo(creep.room.storage);
             }
         }
     }
